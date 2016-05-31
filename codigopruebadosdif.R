4cf375a1f984b186767745d7071146cc239990cd
scoutfranciscano <scoutfranciscano@hotmail.com>
2016-05-31 07:43:15 -0500

    ahora le quite un pedazo y le cambie otro

--- codigopruebadosdif.R
+++ codigopruebadosdif.R
@@ -10,10 +10,26 @@ reg
 
 ##############
 
+# Un comentario 
+
+# laaaargo
+
+# muuuy largo
+
+# por aqui. 
+
+# luego sigue igual
+
+#############
+
 x<-2*runif(1000,10,100)-error
 
 y<-100+2*x+error
 
-reg<-lm(y~x-1)
+reg<-lm(y~x)
 
-reg
+##############
+
+# Un comentario por aqui. luego sigue igual
+
+#############
